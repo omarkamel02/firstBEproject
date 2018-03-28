1b21c9dae243f11c035f494ae19fcfe25d74531f
Unknown <omarko.kamel@gmail.com>
2018-03-28 04:11:47 +0300

    added a models file

---
+++ Forums/models.py
@@ -0,0 +1,12 @@
+class Member :
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+
+
+
+class post :
+    def __init__(self,head,content):
+        self.head=head
+        self.content=content
+        

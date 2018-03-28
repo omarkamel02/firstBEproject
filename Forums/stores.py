1cfbf47c6cab8708f4bdbe4e3af8e1bda8026f66
Unknown <omarko.kamel@gmail.com>
2018-03-28 04:27:53 +0300

    added a stores file

---
+++ Forums/stores.py
@@ -0,0 +1,21 @@
+class MemberStore:
+
+  members = []
+
+  def get_all(self):
+     return members
+
+  def add(self, member):
+      members.append(member)
+
+  def get_by_id(self, id):
+      # search for member by id
+
+  def update(self, member):
+     # update member data
+
+  def delete(self, id):
+      # delete member by id
+
+  def entity_exists(self, member):
+      # checks if an entity exists in a store

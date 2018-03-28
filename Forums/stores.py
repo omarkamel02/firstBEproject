85d73ce058e5bfb1d360c24b958a03d6068b1f62
Unknown <omarko.kamel@gmail.com>
2018-03-28 04:30:18 +0300

    added a store for posts

--- Forums/stores.py
+++ Forums/stores.py
@@ -19,3 +19,25 @@ class MemberStore:
 
   def entity_exists(self, member):
       # checks if an entity exists in a store
+
+class PostsStore:
+
+  posts = []
+
+  def get_all(self):
+     return posts
+
+  def add(self, post):
+      members.append(post)
+
+  def get_by_id(self, id):
+      # search for member by id
+
+  def update(self, post):
+     # update member data
+
+  def delete(self, id):
+      # delete member by id
+
+  def entity_exists(self, post):
+      # checks if an entity exists in a store
